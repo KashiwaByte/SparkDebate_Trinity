eaa8d8acc9e83064bd8c901c6a0e9434e57599e1
黄柏特 <471314513@qq.com>
2023-09-25 10:35:49 +0800

    allow microphone

--- src/views/AIDebateTrain/Sub/DebateTrain.vue
+++ src/views/AIDebateTrain/Sub/DebateTrain.vue
@@ -5,6 +5,7 @@
 <template>
     <iframe
         src="https://kashiwabyte-sparkdebate.demo.swanhub.co"
+        allow="microphone"
         frameborder="0"
         width="1240"
         height="1000"
